07edef3a11e18f145c519b41b0def94b329ffeac
Niklas <niklas.kalkofe@gmail.com>
2024-10-02 10:01:18 +0200

    v0.4

---
+++ Skripts/Systemintegration/Setup/webserver einrichten.sh
@@ -0,0 +1,10 @@
+# Apache Webserver
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install apache2 -y
+
+# PHP
+sudo apt-get install php -y
+
+# MariaDB Datenbank
+sudo apt install mariadb-server php-mysql -y
