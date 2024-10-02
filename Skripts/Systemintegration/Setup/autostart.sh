7ea6db6611196e93d1411688987d114895104e48
Niklas <niklas.kalkofe@gmail.com>
2024-10-02 10:15:58 +0200

    v0.5

---
+++ Skripts/Systemintegration/Setup/autostart.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install git
+
+git --version
+
+git clone https://github.com/BBS2EMDBSEF/Webserver_projekt.git
+
+cd ~/Skripts/Systemintegration/Setup
+chmod +x autostart.sh
+./autostart.sh
