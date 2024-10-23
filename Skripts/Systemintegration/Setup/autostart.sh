997aaefb1848f680e87b403dbc6545a08f22c522
Kalkofe, Niklas (SE-7/3AFS2K) <niklas.kalkofe@volkswagen.de>
2024-10-23 08:35:35 +0200

    Update autostart.sh

--- Skripts/Systemintegration/Setup/autostart.sh
+++ Skripts/Systemintegration/Setup/autostart.sh
@@ -1,10 +1,10 @@
-sudo apt update
-sudo apt install git
+sudo apt update #Updaten aller installierten Dateien auf den aktuellsten Stand
+sudo apt install git #Installation von git, falls nicht vorhanden
 
 git --version
 
-git clone https://github.com/BBS2EMDBSEF/Webserver_projekt.git
+git clone https://github.com/BBS2EMDBSEF/Webserver_projekt.git #Klonen des Repositories, falls Änderungen commited wurden
 
-cd ~/Skripts/Systemintegration/Setup
-chmod +x webseite_einrichten.sh
-./webseite_einrichten.sh
+cd ~/Skripts/Systemintegration/Setup #Navigation zum Setup Ordner
+chmod +x webseite_einrichten.sh #Veränderung der Berechtigungen, um webseite_einrichten.sh auszuführen
+./webseite_einrichten.sh #Ausführen der webseite_einrichten.sh
