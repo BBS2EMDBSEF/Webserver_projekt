0fb3d0bb5b8521dcbde783e83dab55e763c99019
Kalkofe, Niklas (SE-7/3AFS2K) <niklas.kalkofe@volkswagen.de>
2024-12-04 11:54:33 +0100

    update mysql + php

--- Skripts/Systemintegration/Setup/php_config.sh
+++ Skripts/Systemintegration/Setup/php_config.sh
@@ -7,6 +7,7 @@ echo "========================================="
 # PHP installieren
 echo "PHP und notwendige Module werden installiert..."
 sudo apt install -y php php-mysql libapache2-mod-php
+sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
 
 echo "PHP ist erfolgreich installiert."
 echo "========================================="
