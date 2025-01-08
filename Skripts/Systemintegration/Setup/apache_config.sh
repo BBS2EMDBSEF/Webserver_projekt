61d159e8e15bb5d18e175954ac54d35cd684f817
Kalkofe, Niklas (SE-7/3AFS2K) <niklas.kalkofe@volkswagen.de>
2025-01-08 08:53:37 +0100

    implement website

--- Skripts/Systemintegration/Setup/apache_config.sh
+++ Skripts/Systemintegration/Setup/apache_config.sh
@@ -5,5 +5,12 @@ sudo apt install -y apache2
 sudo systemctl enable apache2
 sudo systemctl start apache2
 
+sudo cp -r Skripts/Anwendungsentwicklung/* /var/www/html/
+
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+sudo systemctl restart apache2
+
 echo "Apache ist erfolgreich installiert und aktiv."
 echo "========================================="
